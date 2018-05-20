15b8b9d7349dfd838d9d72f0efc441a82ca023e7
sebgul <karolcia.i.sebus@gmail.com>
2018-05-20 15:58:58 +0200

    new spring boot project for contract calculator app

---
+++ src/main/java/com/sonalake/contractcalculator/ContractCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.sonalake.contractcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractCalculatorApplication.class, args);
+    }
+}
