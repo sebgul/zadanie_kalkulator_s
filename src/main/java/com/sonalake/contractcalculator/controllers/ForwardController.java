e4ff512eaa1f1b0432deaf9241e774c93851edcb
sebgul <karolcia.i.sebus@gmail.com>
2018-05-22 13:17:40 +0200

    forward controller for angular content

---
+++ src/main/java/com/sonalake/contractcalculator/controllers/ForwardController.java
@@ -0,0 +1,16 @@
+package com.sonalake.contractcalculator.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping
+public class ForwardController {
+
+    @GetMapping(value = "/**/{[path:[^\\.]*}")
+    public ModelAndView forward() {
+        return new ModelAndView("/index.html");
+    }
+}
